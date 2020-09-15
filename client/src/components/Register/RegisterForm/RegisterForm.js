22da524dbffa59d720eb9e8ce146416411402ef2
Phien <phiennh@dgroup.co>
2020-09-15 18:07:09 +0700

    Adding input fields to register modal

--- client/src/components/Register/RegisterForm/RegisterForm.js
+++ client/src/components/Register/RegisterForm/RegisterForm.js
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+
+import "./RegisterForm.css";
 
 const RegisterForm = () => {
-    return (
-        <div>
-            <h2>Register</h2>
+  return (
+    <div className="form-container">
+      <h2 className="form-title">Register</h2>
+      <form className="form-body">
+        <span>Name:</span>
+        <div className="input-field-container">
+          <input className="input-field"></input>
         </div>
-    )
-}
+        <span>Name:</span>
+        <div className="input-field-container">
+          <input className="input-field"></input>
+        </div>
+        <span>Name:</span>
+        <div className="input-field-container">
+          <input className="input-field"></input>
+        </div>
+      </form>
+    </div>
+  );
+};
 
 export default RegisterForm;
