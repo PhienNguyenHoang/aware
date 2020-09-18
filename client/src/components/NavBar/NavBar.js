34d07324f43ef1b4d142b1c59c694e57a35b52cf
Phien <phiennh@dgroup.co>
2020-09-18 16:24:38 +0700

    Adding menu bar and dropdown for menu bar

--- client/src/components/NavBar/NavBar.js
+++ client/src/components/NavBar/NavBar.js
@@ -1,10 +1,12 @@
-import React from "react";
+import React, { Fragment } from "react";
+
 import SearchBar from "./SearchBar/SearchBar";
+import NavBarTopRight from "./TopRight/NavBarTopRight";
 
 import "./NavBar.css";
 
 import logo from "../../Images/logo.png";
-import NavBarTopRight from "./TopRight/NavBarTopRight";
+import dropIcon from "../../Images/arrow.png";
 
 const NavBar = () => {
   return (
@@ -15,7 +17,90 @@ const NavBar = () => {
         <img className="navbar-logo" src={logo}></img>
         <NavBarTopRight />
       </div>
-      <div className="lowerBar"></div>
+      <div className="lowerBar">
+        <div className="menu-container">
+          <div className="flex">
+            <span>Men</span>
+            <div className="dropdown1">
+              <img className="dropIcon" src={dropIcon} />
+              <div className="dropdown-content1">
+                <a href="" className="anchor">
+                  Tops
+                </a>
+                <a href="" className="anchor">
+                  Bottoms
+                </a>
+                <a href="" className="anchor">
+                  Dresses
+                </a>
+                <a href="" className="anchor">
+                  Jackets
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <span>Ladies</span>
+            <div className="dropdown2">
+              <img className="dropIcon" src={dropIcon} />
+              <div className="dropdown-content2">
+                <a href="" className="anchor">
+                  Tops
+                </a>
+                <a href="" className="anchor">
+                  Bottoms
+                </a>
+                <a href="" className="anchor">
+                  Dresses
+                </a>
+                <a href="" className="anchor">
+                  Jackets
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <span>Girls</span>
+            <div className="dropdown3">
+              <img className="dropIcon" src={dropIcon} />
+              <div className="dropdown-content3">
+                <a href="" className="anchor">
+                  Tops
+                </a>
+                <a href="" className="anchor">
+                  Bottoms
+                </a>
+                <a href="" className="anchor">
+                  Dresses
+                </a>
+                <a href="" className="anchor">
+                  Jackets
+                </a>
+              </div>
+            </div>
+          </div>
+          <div className="flex">
+            <span>Boys</span>
+            <div className="dropdown4">
+              <img className="dropIcon" src={dropIcon} />
+              <div className="dropdown-content4">
+                <a href="" className="anchor">
+                  Tops
+                </a>
+                <a href="" className="anchor">
+                  Bottoms
+                </a>
+                <a href="" className="anchor">
+                  Dresses
+                </a>
+                <a href="" className="anchor">
+                  Jackets
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
