2d3b2f1fbaa3a9ba63d98eaf209ffb0b4fccb6fa
Phien <phiennh@dgroup.co>
2020-10-07 18:26:21 +0700

    Add mark completed order feature

--- client/src/components/OrderTab/OrderTab.js
+++ client/src/components/OrderTab/OrderTab.js
@@ -1,22 +1,28 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useMemo } from "react";
 import { connect } from "react-redux";
 import "./OrderTab.css";
-import { getAllOrder } from "../../firebase/firebase";
-import { setOrder } from "../../redux/actions/orderActions";
+import { getAllOrder, markOrderComplete } from "../../firebase/firebase";
+import { setOrder, markComplete } from "../../redux/actions/orderActions";
 import OrderTabDetails from "../OrderTabDetails/OrderTabDetails";
-const OrderTab = ({ setOrder, order: { orders } }) => {
+const OrderTab = ({
+  setOrder,
+  order: { orders, orderReducerStatus },
+  markComplete,
+}) => {
   useEffect(() => {
     const fetchData = async () => {
+      console.log("render")
       const result = await getAllOrder();
-      console.log(result);
       if (result && result.length > 0) {
         setOrder(result);
       }
     };
-
     fetchData();
-  }, []);
-  console.log(orders);
+  }, [orderReducerStatus]);
+  const handleClick = (orderId) => {
+    markComplete(orderId);
+  };
+  console.log(orderReducerStatus);
   let orderMarkUp = orders.map((item) => (
     <OrderTabDetails
       key={item.orderId}
@@ -24,16 +30,18 @@ const OrderTab = ({ setOrder, order: { orders } }) => {
       createdAt={item.createdAt}
       status={item.status}
       products={item.products}
+      handleClick={handleClick}
+      orderReducerStatus={orderReducerStatus}
     />
   ));
   return (
     <div className="order-tab-container">
       <div className="order-tab-title">
-        <span>ORDER ID</span>
-        <span>ORDERED DATE</span>
-        <span>DETAIL</span>
-        <span>TOTAL</span>
-        <span>STATUS</span>
+        <span className="order-tab-id">ORDER ID</span>
+        <span className="order-tab-time">ORDERED DATE</span>
+        <span className="order-tab-product">DETAIL</span>
+        <span className="order-tab-total">TOTAL</span>
+        <span className="order-tab-status">STATUS</span>
       </div>
       {orderMarkUp}
     </div>
@@ -44,4 +52,4 @@ const mapStateToProps = (state) => ({
   order: state.order,
 });
 
-export default connect(mapStateToProps, { setOrder })(OrderTab);
+export default connect(mapStateToProps, { setOrder, markComplete })(OrderTab);
